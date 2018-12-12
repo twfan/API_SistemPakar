e80599494d0f8bf957f711e8d0e9e3798d10464a
taufan <taufan.erlangga@gmail.com>
2018-12-12 14:29:17 +0700

    first init

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php 
+
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASSWORD','');
+	define('DATABASE','sistem_pakar');
+
+	$conn = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+	
+	
+	//if date was incorrect
+	/*
+		date_default_timezone_set("Asia/Jakarta");
+	*/ 
+
+ ?>
